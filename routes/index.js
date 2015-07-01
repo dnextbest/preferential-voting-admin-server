75e6c51e205ff1fcad6bd9f0dd0fd122a0e996f3
dnextbest <dominosz89@gmail.com>
2015-07-02 00:13:43 +0200

    Cleaning project + added mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,25 @@
-var SessionHandler = require('./session')
-, ContentHandler = require('./content')
-, ErrorHandler = require('./error').errorHandler
-, CORSHandler = require('./cors').corsHandler
-, HttpLogger = require('./httpLogger').httpLogger;
+var SessionHandler = require('./session'),
+  ContentHandler = require('./content'),
+  ErrorHandler = require('./error').errorHandler,
+  CORSHandler = require('./cors').corsHandler,
+  HttpLogger = require('./httpLogger').httpLogger;
 
 module.exports = exports = function(app, db) {
 
-    var sessionHandler = new SessionHandler(db);
-    var contentHandler = new ContentHandler(db);
+  var sessionHandler = new SessionHandler(db);
+  var contentHandler = new ContentHandler(db);
 
-    // Middleware to see if a user is logged in
-//    app.use(sessionHandler.isLoggedInMiddleware);
+  // Middleware to see if a user is logged in
+  //app.use(sessionHandler.isLoggedInMiddleware);
 
-    app.use(ErrorHandler);
-    app.use(CORSHandler);
-	app.use(HttpLogger);
+  app.use(ErrorHandler);
+  app.use(CORSHandler);
+  app.use(HttpLogger);
 
-	app.get('/', contentHandler.showMainPage);
-    app.get('/api/voteDefs', contentHandler.getVoteDefs);
-    app.post('/api/voteDefs', contentHandler.insertVoteDef);
-    app.put('/api/voteDefs', contentHandler.updateVoteDef);
-    app.get('/api/voteDefs/:id', contentHandler.getVoteDef);
+  app.get('/', contentHandler.showMainPage);
+  app.get('/api/voteDefs', contentHandler.getVoteDefs);
+  app.post('/api/voteDefs', contentHandler.insertVoteDef);
+  app.put('/api/voteDefs', contentHandler.updateVoteDef);
+  app.get('/api/voteDefs/:id', contentHandler.getVoteDef);
 
 }
